62bc00f8264f26026c9b9739629c81d1e033e637
MakesNine <32879692+MakesNine@users.noreply.github.com>
2019-11-30 12:12:38 -0500

    Remove import Amcharts module

--- src/app/map1/map1.module.ts
+++ src/app/map1/map1.module.ts
@@ -3,15 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { Map1RoutingModule } from './map1-routing.module';
 import { Map1Component } from './map1.component';
-import { AmChartsModule } from '@amcharts/amcharts3-angular';
 
 
 @NgModule({
   declarations: [Map1Component],
   imports: [
     CommonModule,
-    Map1RoutingModule,
-    AmChartsModule
+    Map1RoutingModule
   ]
 })
 export class Map1Module { }
--- src/app/map2/map2.module.ts
+++ src/app/map2/map2.module.ts
@@ -4,14 +4,12 @@ import { CommonModule } from '@angular/common';
 import { Map2RoutingModule } from './map2-routing.module';
 import { Map2Component } from './map2.component';
 
-import { AmChartsModule } from '@amcharts/amcharts3-angular';
 
 @NgModule({
   declarations: [Map2Component],
   imports: [
     CommonModule,
-    Map2RoutingModule,
-    AmChartsModule
+    Map2RoutingModule
   ]
 })
 export class Map2Module { }
